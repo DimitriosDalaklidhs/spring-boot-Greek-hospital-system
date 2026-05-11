582347360d2daa6a6d01dff934720bf051908b41
ddalaklidhs <dalaklidesdemetres@gmail.com>
2026-05-12 00:18:03 +0300

    Update comment in App class for clarity

--- src/main/java/com/example/demo/App.java
+++ src/main/java/com/example/demo/App.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class App {  // keep name App to avoid renaming references
+public class App {  // thats me keeping name App to avoid renaming references
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
